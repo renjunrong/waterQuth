543cdc9e4c17912ee45ade7ca06388e74d90447c
zhoucong <410983459@qq.com>
2021-11-30 10:33:22 +0800

    add：添加目录以及路由

---
+++ src/pages/ProcessCenter/applyForm/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="applyForm">
+     <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="表单" name="first">
+        <formTemp></formTemp>
+      </el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import formTemp from './components/formTemp.vue'
+export default {
+  components: {
+    formTemp
+  },
+  data() {
+    return {
+      activeName: 'first'
+    }
+  },
+  methods:{
+    handleClick(tab, event) {
+        console.log(tab, event);
+      }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
